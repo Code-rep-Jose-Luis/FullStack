749ab20a786593fc7c5ab89933672c182e999730
Jose Luis <rgjl1994@gmail.com>
2023-01-18 10:30:28 +0100

    Añadido el enunciado del ejercicio básico 2

--- ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio2.java
+++ ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio2.java
@@ -1,5 +1,16 @@
 package ejerciciosBasicos1;
 
+/*
+ * 2. Crea un programa con 2 variables de tipo entero con los valores 15 y 4. Para estos valores calcula la
+ *suma, la resta, la multiplicación, la división y el resto. Ten en cuenta que al ser 2 números enteros, el
+ *resultado de la división también será entero. Muestra por pantalla los resultados de esta manera:
+ *15 + 4 = 19
+ *15 – 4 = 11
+ *15 / 4 = 3
+ *15 * 4 = 60
+ *15 % 4 = 3
+ *
+ */
 public class Ejercicio2 {
 
 		private static void operaciones() {
