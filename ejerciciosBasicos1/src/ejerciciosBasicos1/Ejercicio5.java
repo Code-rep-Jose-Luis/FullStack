26d3b6a6b3d33230c0c73550b142669e069c77c0
Jose Luis <rgjl1994@gmail.com>
2023-01-18 12:05:02 +0100

    Añadido el mismo ejercicio con operaciones acortadas

--- ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio5.java
+++ ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio5.java
@@ -28,7 +28,20 @@ public class Ejercicio5 {
 
 	}
 	
+	public static void operaciones2() {
+		int N = 15;
+		System.out.println("Valor inicial de N = "+N);
+		N += 77;
+		System.out.println("N + 77 = "+N);
+		N -= 3;
+		System.out.println("N - 3 = "+N);
+		N *= 2;
+		System.out.println("N * 2 = "+N);
+
+	}
+	
 	public static void main(String[] args) {
 		operaciones();
+		operaciones2();
 	}
 }
