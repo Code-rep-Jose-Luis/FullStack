70adf0da1211adf724fab454455a210a57d7cba6
Jose Luis <rgjl1994@gmail.com>
2023-01-18 09:24:44 +0100

    Corrección en el resultado del ejercicio 3

--- ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio3.java
+++ ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio3.java
@@ -6,14 +6,14 @@ public class Ejercicio3 {
 	private static void comparador() {
 		int a = 5, b = 5;
 		
-		// El valor es mayor
-		System.out.println("El primer valor "+a+" es mayor al segundo "+b+": "+(a>b));
-
 		// El valor es menor
 		System.out.println("El primer valor "+a+" es menor al segundo "+b+": "+(a<b));
 
 		// El valor es igual
 		System.out.println("El primer valor "+a+" es igual al segundo "+b+": "+(a==b));
+
+		// El valor es mayor o igual
+		System.out.println("El primer valor "+a+" es mayor o igual al segundo "+b+": "+(a>=b));
 
 		
 	}
