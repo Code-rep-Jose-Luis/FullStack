260c706e30d47161f7c121939b18db55e0bfa136
Jose Luis <rgjl1994@gmail.com>
2023-01-18 12:01:40 +0100

    Añadido el enunciado del ejercicio

--- ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio3.java
+++ ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio3.java
@@ -1,5 +1,16 @@
 package ejerciciosBasicos1;
 
+/*
+ * 3. Crea 2 variables enteras con el mismo valor en la misma línea (declaración y asignación) y muestra la
+siguiente información:
+a. Si la primera variable es menor que la segunda
+b. Si son iguales
+c. Si la primera variable es mayor o igual que la segunda
+Muestra el resultado indicando si la comparación es True o False (valor del booleano). Ejemplo: 6 es menor
+que 6: False
+
+*/
+
 public class Ejercicio3 {
 
 	
