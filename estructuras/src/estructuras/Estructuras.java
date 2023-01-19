07fb65032951ae5a8c4ac27fda25f1c771d81f50
Jose Luis <rgjl1994@gmail.com>
2023-01-19 11:29:36 +0100

    Añadir Bloque para estructuras

---
+++ estructuras/src/estructuras/Estructuras.java
@@ -0,0 +1,18 @@
+package estructuras;
+
+public class Estructuras {
+
+	public static void ejemploIf() {
+		String nombre = "Jose";
+		if(nombre.equals("Jose")) {
+			System.out.println("Hola "+nombre);
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		ejemploIf();
+
+	}
+
+}
