c212376cbbc64f3411a2aafcbdd4dc7cf7f3dea5
Jose Luis <rgjl1994@gmail.com>
2023-01-19 12:15:51 +0100

    Añadido ejemplos de if else, if else if

--- estructuras/src/estructuras/Estructuras.java
+++ estructuras/src/estructuras/Estructuras.java
@@ -7,17 +7,58 @@ public class Estructuras {
 	public static void ejemploIf() {
 		//String nombre = "Jose";
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca el nombre: ");
 		String nombre = sc.nextLine();
 		if(nombre.equals("Jose")) {
 			System.out.println("Hola "+nombre);
 		}
 		System.out.println("Pasa un buen dia!");
-
+		sc.close();
+	}
+	public static void ejemploIfElse() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca el nombre: ");
+		String nombre = sc.nextLine();
+		if(nombre.equals("Jose")) {
+			System.out.println("Hola "+nombre);
+			System.out.println("Pasa un buen dia!");
+		}else {
+			System.out.println("Pasa un buen dia");
+		}
+		sc.close();
+	}
+	public static void ejemploIfElseIfElse() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca el numero del dado: ");
+		int numero = sc.nextInt();
+		if(numero == 1) {
+			System.out.println(numero);
+		}
+		else if(numero == 2) {
+			System.out.println(numero);
+		}
+		else if(numero == 3) {
+			System.out.println(numero);
+		}
+		else if(numero == 4) {
+			System.out.println(numero);
+		}
+		else if(numero == 5) {
+			System.out.println(numero);
+		}
+		else if(numero == 6) {
+			System.out.println(numero);
+		}
+		else {
+			System.out.println("El numero no es una cara del dado");			
+		}
 		sc.close();
 	}
 	
 	public static void main(String[] args) {
-		ejemploIf();
+		//ejemploIf();
+		//ejemploIfElse();
+		ejemploIfElseIfElse();
 
 	}
 
