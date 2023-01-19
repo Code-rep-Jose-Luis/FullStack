670c8ba6ede6f78b54fa4cfac5bb8777cfaa0607
Jose Luis <rgjl1994@gmail.com>
2023-01-19 11:36:55 +0100

    Añadido ejemplo If

--- estructuras/src/estructuras/Estructuras.java
+++ estructuras/src/estructuras/Estructuras.java
@@ -1,13 +1,19 @@
 package estructuras;
 
+import java.util.Scanner;
+
 public class Estructuras {
 
 	public static void ejemploIf() {
-		String nombre = "Jose";
+		//String nombre = "Jose";
+		Scanner sc = new Scanner(System.in);
+		String nombre = sc.nextLine();
 		if(nombre.equals("Jose")) {
 			System.out.println("Hola "+nombre);
 		}
+		System.out.println("Pasa un buen dia!");
 
+		sc.close();
 	}
 	
 	public static void main(String[] args) {
