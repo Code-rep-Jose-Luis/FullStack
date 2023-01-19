6aee30683dede834e7ab281510937d85c3a24ba7
Jose Luis <rgjl1994@gmail.com>
2023-01-19 13:14:47 +0100

    Añadido operación ternaria

--- estructuras/src/estructuras/Estructuras.java
+++ estructuras/src/estructuras/Estructuras.java
@@ -108,12 +108,25 @@ public class Estructuras {
 		sc.close();
 	}
 	
+	public static void ejemplosTernarios() {
+		int edad = 10;
+		if(edad>=18) {
+			System.out.println("Eres mayor de edad");
+		}else {
+			System.out.println("Eres menor de edad");			
+			}
+
+		System.out.println(edad>=18?"Eres mayor de edad":"Eres menor de edad");
+		
+	}
+	
 	public static void main(String[] args) {
 		//ejemploIf();
 		//ejemploIfElse();
 		//ejemploIfElseIfElse();
 		//IfElseIfSinElse();
-		ejemploSwitch();
+		//ejemploSwitch();
+		ejemplosTernarios();
 	}
 
 }
