8d766cd155ed3a9c3315fd3edd78edca423e64e3
Jose Luis <rgjl1994@gmail.com>
2023-01-20 08:52:56 +0100

    Añadido Ejercicio1

--- ejerciciosEstructuras/src/ejerciciosEstructuras/Ejercicio1.java
+++ ejerciciosEstructuras/src/ejerciciosEstructuras/Ejercicio1.java
@@ -1,5 +1,22 @@
 package ejerciciosEstructuras;
 
+/**
+ * 
+ * @author José Luis Romero Gallego
+ *
+ * 1.Crear un programa que me diga si un numero es par o impar
+ */
 public class Ejercicio1 {
 
+	public static void operacione() {
+		int numero = 8;
+		System.out.println(((numero%2>0)?(numero+" es impar"):(numero+" es par")));
+		numero = 7;
+		System.out.println(((numero%2>0)?(numero+" es impar"):(numero+" es par")));
+
+	}
+	
+	public static void main(String[] args) {
+		operacione();
+	}
 }
