b89fe1275a22c4f887ca6f154c22965a4af67a4a
Jose Luis <rgjl1994@gmail.com>
2023-01-20 08:41:42 +0100

    Pequeña modificación del ejercicio 4

--- ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio4.java
+++ ejerciciosBasicos1/src/ejerciciosBasicos1/Ejercicio4.java
@@ -1,12 +1,12 @@
 package ejerciciosBasicos1;
 
 /*
-4. Crea 4 variables de tipo double con los valores que quieras. A continuación muestra sus valores por
-consola usando 4 instrucciones para imprimir por consola y sin generar ningún salto de línea (todos
-los valores se verán en una sola línea
-A continuación, muestra el resultado de sumar las 2 primeras variables, multiplicar el resultado
-por la tercera y dividir por la cuarta. Pista: la suma tiene menos prioridad que la multiplicación, por lo
-que necesitarás agrupar dicha operación entre paréntesis.
+ * 4. Crea 4 variables de tipo double con los valores que quieras. A continuación muestra sus valores por
+ * consola usando 4 instrucciones para imprimir por consola y sin generar ningún salto de línea (todos
+ * los valores se verán en una sola línea
+ * A continuación, muestra el resultado de sumar las 2 primeras variables, multiplicar el resultado
+ * por la tercera y dividir por la cuarta. Pista: la suma tiene menos prioridad que la multiplicación, por lo
+ * que necesitarás agrupar dicha operación entre paréntesis.
  */
 
 public class Ejercicio4 {
@@ -22,7 +22,7 @@ public class Ejercicio4 {
 		System.out.print(numero3);
 		System.out.print(numero4);
 		
-		System.out.print("\n"+((numero1+numero2)*numero3)/numero4);
+		System.out.print("\n"+(numero1+numero2)*numero3/numero4);
 	}
 	
 	public static void main(String[] args) {
