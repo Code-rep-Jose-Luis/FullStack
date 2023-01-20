ec09f1e092525cc647931b40797f6d51febb7d58
Jose Luis <rgjl1994@gmail.com>
2023-01-20 13:43:09 +0100

    Añadido Ejercicio 13

---
+++ ejerciciosEstructuras/src/ejerciciosEstructuras/Ejercicio13.java
@@ -0,0 +1,32 @@
+package ejerciciosEstructuras;
+
+import java.util.Iterator;
+
+/**
+ * 13. Muestra por pantalla los números de 20 al 1 usando un bucle while, y después hazlo otra vez
+ * usando un bucle for.
+ * @author José Luis Romero Gallego
+ *
+ */
+
+public class Ejercicio13 {
+
+	public static void operacion(){
+		int numero = 20;
+		while(numero>0) {
+			System.out.println(numero);
+			numero--;
+		}
+		
+		System.out.println("----");	
+		numero = 20;
+		for (int i = numero; i > 0 ; i--) {
+			System.out.println(i);	
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		operacion();
+	}
+}
