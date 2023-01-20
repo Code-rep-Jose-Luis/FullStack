47b53ba215f1f053f28b07eeb04751475bbac303
Jose Luis <rgjl1994@gmail.com>
2023-01-20 09:23:23 +0100

    Añadido el ejercicio 4

---
+++ ejerciciosEstructuras/src/ejerciciosEstructuras/Ejercicio4.java
@@ -0,0 +1,29 @@
+package ejerciciosEstructuras;
+
+import java.util.Scanner;
+
+/*
+ * 4. Programa que le pida 2 cadenas de texto al usuario y compruebe si son iguales.
+ */
+
+public class Ejercicio4 {
+
+	public static void operacion() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un texto: ");
+		String cadena1=sc.nextLine();
+		System.out.println("Introduce un segundo texto: ");
+		String cadena2=sc.nextLine();
+
+		// opcion 1 cadenas identicas
+		System.out.println((cadena1.equals(cadena2))?"Las 2 cadenas son iguales: "+cadena1:"Las cadenas son distintas '"+cadena1+"' | '"+cadena2+"'");
+		// opcion 2 cadenas identicas ignorando mayusculas/minusculas
+		System.out.println((cadena1.equalsIgnoreCase(cadena2))?"Las 2 cadenas tienen las mismas letras ignorando mayusculas y minusculas: "+cadena1.toLowerCase():"Las cadenas son distintas '"+cadena1+"' | '"+cadena2+"'");
+		sc.close();
+	}
+	
+	public static void main(String[] args) {
+		operacion();
+	}
+	
+}
