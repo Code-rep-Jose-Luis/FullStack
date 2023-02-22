ae78d507cab8081627c269c71a05fa7909c0b568
Jose Luis <rgjl1994@gmail.com>
2023-02-22 12:31:02 +0100

    re format

--- ejercicioBancos/src/main/java/com/jose/ejercicioBancos/App.java
+++ ejercicioBancos/src/main/java/com/jose/ejercicioBancos/App.java
@@ -24,9 +24,9 @@ import com.jose.ejercicioBancos.entidades.Oferta;
 import com.jose.ejercicioBancos.entidades.Ofertas;
 
 public class App {
-	
+
 	private static Scanner sc;
-	
+
 	/**
 	 * Funcion que devuelve todas las lineas de un archivo en filas
 	 * 
@@ -118,10 +118,10 @@ public class App {
 	 */
 	public static Cuentas devolverCliente(Cuentas cuentas) {
 		Cuentas cuentasCliente = new Cuentas();
-		
+
 		ArrayList<String> dni = new ArrayList<>();
 		dni.add("");
-		String cadena="";
+		String cadena = "";
 		do {
 			System.out.println("Introduce el dni de una cuenta existente: ");
 			dni.set(0, sc.nextLine());
@@ -129,7 +129,6 @@ public class App {
 					.addCuentas(cuentas.getCuentas().stream().filter(e -> e.getDni().equals(dni.get(0))).toList());
 		} while (cuentasCliente.getCuentas().size() == 0);
 
-		
 		return cuentasCliente;
 	}
 
@@ -166,28 +165,27 @@ public class App {
 		System.out.println(LocalDateTime.now()
 				.format(DateTimeFormatter.ofPattern("'Date: 'eeee',' MMMM dd',' yyyy\n'Time: 'HH:mm:ss", Locale.UK)));
 	}
-	
+
 	public static LocalDate fechaNacimiento(Cuentas cliente) {
-		List<Cuenta> clientesFecha=cliente.getCuentas().stream().distinct().toList();
-		LocalDate fecha=clientesFecha.get(0).getfechaNacimiento();
-		if (clientesFecha.size()>1) {
+		List<Cuenta> clientesFecha = cliente.getCuentas().stream().distinct().toList();
+		LocalDate fecha = clientesFecha.get(0).getfechaNacimiento();
+		if (clientesFecha.size() > 1) {
 			System.out.println("Elige la fecha de nacimiento correcta");
 			for (int i = 0; i < clientesFecha.size(); i++) {
-				System.out.println((i+1)+". "+clientesFecha.get(i).getfechaNacimiento());
+				System.out.println((i + 1) + ". " + clientesFecha.get(i).getfechaNacimiento());
 			}
-			int eleccion=0;
-			while (eleccion<1 || eleccion>clientesFecha.size()){
+			int eleccion = 0;
+			while (eleccion < 1 || eleccion > clientesFecha.size()) {
 				System.out.println("Introduce opcion: ");
-				eleccion=Integer.parseInt(sc.nextLine());
+				eleccion = Integer.parseInt(sc.nextLine());
 			}
-			
-			fecha=clientesFecha.get(eleccion-1).getfechaNacimiento();
+
+			fecha = clientesFecha.get(eleccion - 1).getfechaNacimiento();
 		}
-		
+
 		return fecha;
 	}
 
-	
 	/**
 	 * Funcion para añadir una Cuenta de CuentaSantander a una lista en Cuentas
 	 * 
@@ -198,45 +196,37 @@ public class App {
 		List<Oferta> ofertas = new ArrayList<>();
 		for (String s : datosDevueltos) {
 			String[] datos = s.split(";");
-			ofertas.add(new Oferta(Integer.parseInt(datos[0]),Integer.parseInt(datos[1]),
-					Double.parseDouble(datos[2]),Double.parseDouble(datos[3]),
-					datos[4]));
+			ofertas.add(new Oferta(Integer.parseInt(datos[0]), Integer.parseInt(datos[1]), Double.parseDouble(datos[2]),
+					Double.parseDouble(datos[3]), datos[4]));
 		}
 		return ofertas;
 	}
+
 	/**
-	 * Funcion que devuelve la oferta que este en el rango de edad y saldo, devolviendo la que mayor saldo minimo tenga
+	 * Funcion que devuelve la oferta que este en el rango de edad y saldo,
+	 * devolviendo la que mayor saldo minimo tenga
+	 * 
 	 * @param fechaNacimiento
 	 * @param cuentasCliente
 	 * @return
 	 */
 	public static Oferta obtenerOferta(LocalDate fechaNacimiento, Cuentas cuentasCliente) {
-		Ofertas ofertas=new Ofertas(devolverOfertas(devolverLineasJava8(Paths.get("archivos", "productosofertados.txt"))));
-		double saldoCuentas=cuentasCliente.getCuentas().stream().mapToDouble(e->e.getSaldo()).sum();
-		int edad=Period.between(fechaNacimiento, LocalDate.now()).getYears();
-		List<Oferta> oferta=ofertas.getOferta(edad, saldoCuentas);
+		Ofertas ofertas = new Ofertas(
+				devolverOfertas(devolverLineasJava8(Paths.get("archivos", "productosofertados.txt"))));
+		double saldoCuentas = cuentasCliente.getCuentas().stream().mapToDouble(e -> e.getSaldo()).sum();
+		int edad = Period.between(fechaNacimiento, LocalDate.now()).getYears();
+		List<Oferta> oferta = ofertas.getOferta(edad, saldoCuentas);
 		return oferta.stream().max(Comparator.comparingDouble(Oferta::getSaldoMin)).orElseThrow();
-		
+
 	}
+
 	/**
-	 * Funcion que llama a los mensajes segun la el pais del cliente
+	 * Funcion que llama a los mensajes segun la el pais del cliente y muestra la
+	 * oferta
+	 * 
 	 * @param cuentasCliente
 	 */
-	public static void mensajes(Cuentas cuentasCliente) {
-		if (cuentasCliente.getCuentas().get(0).getcodigoPais().equalsIgnoreCase("ES")) {
-			mensajeEs(cuentasCliente.getCuentas().get(0));
-			fechaEs();
-		} else {
-			mensajeEx(cuentasCliente.getCuentas().get(0));
-			fechaEx();
-		}
-		
-	}
-	/**
-	 * Funcion que llama a los mensajes segun la el pais del cliente y muestra la oferta
-	 * @param cuentasCliente
-	 */
-	public static void mensajes(Cuentas cuentasCliente,Oferta oferta) {
+	public static void mensajes(Cuentas cuentasCliente, Oferta oferta) {
 		if (cuentasCliente.getCuentas().get(0).getcodigoPais().equalsIgnoreCase("ES")) {
 			mensajeEs(cuentasCliente.getCuentas().get(0));
 			fechaEs();
@@ -245,18 +235,15 @@ public class App {
 			fechaEx();
 		}
 		System.out.println(oferta.getNombre());
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
 		Cuentas cuentas = devolverCuentas();
 		Cuentas cuentasCliente = devolverCliente(cuentas);
-		Oferta oferta=obtenerOferta(fechaNacimiento(cuentasCliente),cuentasCliente);
-		mensajes(cuentasCliente,oferta);
+		mensajes(cuentasCliente, obtenerOferta(fechaNacimiento(cuentasCliente), cuentasCliente));
 		sc.close();
 	}
-
-
 
 }
